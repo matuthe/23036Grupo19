4e02994862ae329f61ca1fd432ad10f4070419c2
Grupo19 <scarpinomatias@outlook.com>
2023-05-13 15:29:13 -0300

    mati

--- javascript/js.js
+++ javascript/js.js
@@ -1,1 +1,17 @@
-console.log("Hola")
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 700,
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 3,
+        indicators: true,
+        noWrap : true,
+        
+
+
+
+        });
+
+});
