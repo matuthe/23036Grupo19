de650325e0498c6a3c5811242c4c0e2dce3066c5
MSoledad <msoledad.sanchez@hotmail.com>
2023-05-16 16:21:54 -0300

    MSS Act Prod

---
+++ javascript/productos.js
@@ -0,0 +1,21 @@
+/* let fotos=[
+    "../img/remera1.jpg",
+    "../img/remera2.jpg",
+    "../img/remera3.jpg",
+    "../img/remera4.jpg",
+] */
+
+let productos= [
+{
+id: 1,
+titulo: "Remera Nike",
+descripcion: "Remera Nike color azul",
+category: "Indumentaria",
+subcategory: "Remeras",
+precio: "3500",
+image: "../img/remera1.jpg",
+cuotas: "si",
+},
+{
+}
+]
