e507a66e4114db01d5ac0f1d7eee5f204c739adc
MSoledadSanchez <msoledad.sanchez@hotmail.com>
2023-05-21 21:43:56 -0300

    MSS
    Carga la galeria con Array

---
+++ javascript/cargar_pro.js
@@ -0,0 +1,66 @@
+//* Cargo los productos en la galeria */
+
+document.addEventListener("DOMContentLoaded",function(event){
+
+
+let precioReal 
+let precioLista 
+
+let cad = `<div class="container_card">
+`
+productos.forEach(function(item) {
+
+    precioReal = item.precio
+    precioLista = ""
+    // console.log (Number(item.descuento))
+    cad += `
+        <div class="card" category="${item.category}">
+            <img src="${item.image}" alt="${item.descripcion}" title="${item.titulo}" itemprop="image" width="75%" height="75%">
+            <div class="card_info">
+                <div class="card_linea">${item.titulo}`
+                
+            if(Number(item.descuento)!=0){
+                cad += `<span class="card_linea1">
+                        <span class="material-icons-outlined">discount</span>
+                        ${item.descuento} %</span>`
+                precioReal = item.precio - (item.precio * item.descuento / 100)
+               
+                precioLista = "  $" + item.precio
+            }
+
+        cad += `</div>
+        <div class="card_linea">$ ${precioReal}<span class="card_linea2">${precioLista}</span> 
+        </div> `
+                
+        if(Number(item.cuotas)!=0){
+                cad += `<div class="card_linea">
+                    <span class="material-icons-outlined card_linea3">credit_card</span>
+                    ${item.cuotas} de $ ${Math.round(item.precio/item.cuotas)}</div>`};
+ 
+    cad += `</div></div>`;
+})
+
+cad += `</div>`
+
+
+// document.write(cad)
+// document.getElementById("cargar_galeria").innerHTML = cad;
+
+
+    document.getElementById('cargar_galeria').innerHTML= cad;
+    
+});
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
--- javascript/productos.js
+++ javascript/productos.js
@@ -5,7 +5,7 @@
     "../img/remera4.jpg",
 ] */
 
-let productos= [
+const productos= [
 {
 id: 1,
 titulo: "Remera New Balance",
@@ -39,53 +39,137 @@ image: "../img/remera3.jpg",
 cuotas: "3",
 descuento: "30"
 },
+{
+    id: 4,
+    titulo: "Remera Under Armour",
+    descripcion: "Driy Fit Azul Francia",
+    category: "Indumentaria",
+    subcategory: "Remeras", 
+    precio: "3500",
+    image: "../img/remera4.jpg",
+    cuotas: "0",
+    descuento: "10"
+    },
+{
+    id: 5,
+    titulo: "Remera Nike",
+    descripcion: "Algodon Celeste",
+    category: "Indumentaria",
+    subcategory: "Remeras", 
+    precio: "2800",
+    image: "../img/remera5.jpg",
+    cuotas: "2",
+    descuento: "0"
+    },
+
+    {
+        id: 6,
+        titulo: "Remera Adidas",
+        descripcion: "Roja con logo",
+        category: "Indumentaria",
+        subcategory: "Remeras", 
+        precio: "3500",
+        image: "../img/remera6.jpg",
+        cuotas: "0",
+        descuento: "0"
+        },
+
+{
+        id: 7,
+        titulo: "Zapatilla New Balance",
+        descripcion: "Urban Gris",
+        category: "Calzado",
+        subcategory: "Zapatillas", 
+        precio: "10500",
+        image: "../img/zapa1.jpg",
+        cuotas: "6",
+        descuento: "0"
+        },
+
+{
+        id: 8,
+        titulo: "Zapatilla Puma",
+        descripcion: "Urban Blanca",
+        category: "Calzado",
+        subcategory: "Zapatillas", 
+        precio: "9000",
+        image: "../img/zapa2.jpg",
+        cuotas: "0",
+        descuento: "0"
+        },
+{
+        id: 9,
+        titulo: "Zapatilla Nike",
+        descripcion: "Deportiva Negra",
+        category: "Calzado",
+        subcategory: "Zapatillas", 
+        precio: "12000",
+        image: "../img/zapa3.jpg",
+        cuotas: "3",
+        descuento: "0"
+        },
+{
+        id: 10,
+        titulo: "Zapatilla Adidas",
+        descripcion: "Deportiva Turquesa",
+        category: "Calzado",
+        subcategory: "Zapatillas", 
+        precio: "15000",
+        image: "../img/zapa4.jpg",
+        cuotas: "3",
+        descuento: "20"
+        },
+
+
+    {
+        id: 11,
+        titulo: "Pelota Qatar 2022",
+        descripcion: "La pelota de los campeones",
+        category: "Accesorios",
+        subcategory: "Pelotas", 
+        precio: "6000",
+        image: "../img/accesorio1.jpg",
+        cuotas: "0",
+        descuento: "0"
+        },
+
+ {
+        id: 12,
+        titulo: "Bolso Nike",
+        descripcion: "Mediano Rosa y Negro",
+        category: "Accesorios",
+        subcategory: "Bolsos", 
+        precio: "6200",
+        image: "../img/accesorio2.jpg",
+        cuotas: "0",
+        descuento: "10"
+        },
+
+     {
+        id: 13,
+        titulo: "Gorro Nike",
+        descripcion: "Blanco con logo",
+        category: "Accesorios",
+        subcategory: "Gorras", 
+        precio: "2000",
+        image: "../img/accesorio3.jpg",
+        cuotas: "0",
+        descuento: "0"
+        },
+
+     {
+        id: 14,
+        titulo: "Gorro Jordan",
+        descripcion: "Rojo con logo gris",
+        category: "Accesorios",
+        subcategory: "Gorras", 
+        precio: "2200",
+        image: "../img/accesorio4.jpg",
+        cuotas: "2",
+        descuento: "0"
+        },
+
+
+
+        
 ]
-
-
-function cargarProductos() {
-
-    let cad = `<div class="container_card">
-`
-    productos.forEach(function(item) {
-
-        // console.log (Number(item.descuento))
-        cad += `
-            <div class="card" category="${item.category}">
-                <img src="${item.image}" alt="${item.descripcion}" title="${item.titulo}" itemprop="image" width="75%" height="75%">
-                <p>${item.titulo}`
-                
-                if(Number(item.descuento)!=0){
-                    cad += `<span style="color:red">
-                            <span class="material-icons-outlined">discount</span>
-                            ${item.descuento} %</span>`};
-
-                cad += `</p>
-                        <p>${item.precio}</p>`
-                
-                if(Number(item.cuotas)!=0){
-                    cad += `<p>
-                            <span class="material-icons-outlined">credit_card</span>
-                            ${item.cuotas} de $ ${item.precio/item.cuotas}</p>`};
-
-            cad += `</div>`;
-        })
-
-    cad += `</div>`
-    
-     
-    // document.write(cad)
-    document.getElementById("cargar_galeria").innerHTML = cad;
-    
-    console.log (cad);
-}
-
-cargarProductos()
-
- 
-
-
-
-
-
-
-
